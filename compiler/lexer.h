836cc2c39827afcc7efe9d59925b04cd4070d81b
Mark-Gibson-44 <mjgibson44@outlook.com>
2020-11-21 17:35:38 +0000

    Added to the lexer some more as well as develop some of the later stages of the compiler, Next steps would be implementing a grammar to fulfil the lexer. Will also do a directory clean-up

--- compiler/lexer.h
+++ compiler/lexer.h
@@ -1,77 +1,113 @@
 #pragma once
+
+#include <iostream>
 #include <fstream>
 
 class lexer
 {
+	std::fstream f;
 	bool begin = false;
 	char prevChar;
 	char current;
 	std::string curLexeme;
+	//std::vector<lexeme>
+	public:
 	void read()
 	{
-		current = //grab next character
-		processChar(current)// will only do something if char = { or ; currently
+		
+		f.open("./test.txt");
+		while(f.get(current)){
+		//grab next character
+		curLexeme += current;
+		processChar(current);// will only do something if char = { or ; currently
+		
 		if(!begin)//if begin is false prevChar doesn't exist
 		{
 			
 			prevChar = current;
 		}
 		//Read first character -> process character
-	}
-	//reads from after an openning brace
-	//returns true if there is a corresponding closing brace
-	//if another opening brace appears recursively calls the function
-	//takes a pointer to a position in a file?
-	bool closingBrace()
-	{
-		char next;
-		if(next == '{')//just recursively go through the file for now
-			closeBrace();
-		if(next == '}')
-			return true;
+		}
 	}
 
+	//reads from after an openning brace
+	//Will be responsible for scope management to some extent, could potentially be used in semantic segmentation rather than 
+	//lexing
+	//if another opening brace appears recursively calls the function
+	//takes a pointer to a position in a file?
+	void closingBrace(std::fstream &b)
+	{
+		std::string innerLex;
+		while(b.get(current))
+		{
+			
+			curLexeme += current;
+			innerLex += current;
+			if(current == '{')//just recursively go through the file for now
+				closingBrace(b);
+			if(current == '}')
+			{
+				std::cout << innerLex << std::endl;
+				return;
+			}
+		}
+
+	}
+	//Just processes special characters
 	void processChar(char c)
 	{
 		switch(c)
 		{
-			case ';':
-				processLexeme(curLexeme);
+			case ';'://stmt endings
+				processLexeme();
 				curLexeme = ""; //reset currentLexeme
 				break;
-			case '{':
-				closingBrace();
+			case '{'://scope initialising
+				closingBrace(f);
 				break;
-			case '+':
+			case '+'://arithmetic operations
 			case '-':
 			case '/':
 			case '*':
-				evalOp(); //indicates a mathematical operation (if concat isnt allowed) have function that checks theres a value after
+							
+				finishLexeme(f); //indicates a mathematical operation (if concat isnt allowed) have function that checks theres a value after
 				break;
 		}
 
 	}
-	//probably takes another pointer to a point in a file
-	//reads up until a ;
-	void finishLexeme()
+	//Uses a grammar of lexemes to deteremine if the current is valid
+	//Will have to generate a grammar.h to generate valid strings
+	bool validLexeme()
 	{
-		char pos;
-		while(pos != ';')
-		{
-			curLexeme += pos;
-			pos = getChar();
-		}
+	//
+	//
+		return true;
+		
+	}
+	//Error checking of the current lexeme;
+	
+	void processLexeme()
+	{
+		std::cout << curLexeme;
+		if(!validLexeme())
+			std::cerr << "Invalid Lexeme" << std::endl;
 		
 	}
 	//Run through the current string
 	//check that its either TYPE var1 operator
 	//then peek ahead to the next character up until a ;
-	//return true if all is as should be
-	bool evalOp()//decompose the current string and make sure it is a valid expression
-	{
-		finishLexeme();//finishes the lexeme providing all the information to check if the expr is valid
-		//probably use the AST here
-			
+	
+	void finishLexeme(std::fstream& b)
+	{	
+		
+		while(b.get(current))
+		{
+		curLexeme += current;
+		
+			if(current == ';')
+				break;
+		}	
+		
 	}
 
 };
